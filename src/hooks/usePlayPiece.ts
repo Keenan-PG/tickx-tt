242f4f25580407cd6b73d984da711c192e18c20e
Keenan-PG <kgolding8991@hotmail.com>
2022-11-11 16:58:48 +0000

    testing data output / figuring current wins

--- src/hooks/usePlayPiece.ts
+++ src/hooks/usePlayPiece.ts
@@ -3,6 +3,9 @@ import { useRecoilState } from "recoil";
 import { boardState, gameOverState, playerState } from "state";
 
 const testWin = (arr: number[]): boolean => /1{4}|2{4}/.test(arr.join(""));
+// TODO: update regex above to account for diagonal
+// example: diagonal right etc
+// example win: (player 1, diagonal right simple - joined board): 1,22,12,1,12,1,2,1
 
 const usePlayPiece = () => {
   const [board, setBoard] = useRecoilState(boardState);
@@ -36,6 +39,14 @@ const usePlayPiece = () => {
     } else {
       setPlayerTurn(player === 1 ? 2 : 1);
     }
+
+    // figuring out structure / how wins are done
+    console.log('newBoard[col]', newBoard[col])
+    console.log('newBoard[col] joined', newBoard[col].join("")) // passed into testWin and joined before regex in testWin
+    console.log('newBoard.map((col) => col[row] || 0)', newBoard.map((col) => col[row] || 0)) 
+    console.log('newBoard.map((col) => col[row] || 0) joined', newBoard.map((col) => col[row] || 0).join("")) // passed into testWin and joined before regex in testWin
+    console.log('newBoard', newBoard) // testing what newboard looks like
+    console.log('newBoard joined', newBoard.join("")) // testing what newboard joined looks like
 
     setBoard(newBoard);
   };
